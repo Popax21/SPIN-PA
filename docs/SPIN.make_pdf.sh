64641d8e9e41aa85823519d54becadcdd3090fc2
Popax21 <popa21@popa21.at>
2023-03-31 22:40:56 +0200

    Fix typo in title

--- docs/SPIN.make_pdf.sh
+++ docs/SPIN.make_pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-cat SPIN.prefix Celeste\ Spinner\ Analyisis\ -\ the\ SPIN\ theory.md | grep -v '^{}{}' | pandoc --shift-heading-level-by=-1 --pdf-engine=xelatex -f markdown -t pdf -o SPIN.pdf
+cat SPIN.prefix Celeste\ Spinner\ Analysis\ -\ the\ SPIN\ theory.md | grep -v '^{}{}' | pandoc --shift-heading-level-by=-1 --pdf-engine=xelatex -f markdown -t pdf -o SPIN.pdf

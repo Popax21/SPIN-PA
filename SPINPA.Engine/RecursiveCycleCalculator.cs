94e80786bba02252e4ebcf8dbf13b82321f3a749
Popax21 <popa21@popa21.at>
2023-04-02 02:01:58 +0200

    Fix RecursiveCycleCalculator.CalcCycleGroup

--- SPINPA.Engine/RecursiveCycleCalculator.cs
+++ SPINPA.Engine/RecursiveCycleCalculator.cs
@@ -3,6 +3,18 @@ using System.Numerics;
 namespace SPINPA.Engine;
 
 public static class RecursiveCycleCalculator {
+    public static long CalcCycleGroup(BigRational interv, BigRational delta, BigRational off) {
+        off = CalcUtils.RMod(off, interv);
+
+        //Magical edge cases
+        if(delta > 0 && off - interv > -BigRational.Abs(delta)) off -= interv;
+        if(delta < 0 && off > 0) off -= interv;
+
+        long group = long.Abs((long) BigRational.Ceiling(off / BigRational.Abs(delta)));
+        Assert.AssertTrue(0 <= group && group <= CalcCycleLength(interv, delta));
+        return group;
+    }
+
     public static long CalcCycleLength(BigRational interv, BigRational delta) {
         BigRational f = interv / BigRational.Abs(delta);
         if(1 < f && f < 2) return 2;
@@ -12,7 +24,8 @@ public static class RecursiveCycleCalculator {
     public static BigRational CalcResidualDrift(BigRational interv, BigRational delta) => (interv - CalcCycleLength(interv, delta) * BigRational.Abs(delta));
     public static BigRational CalcOffsetDrift(BigRational interv, BigRational delta, long numFrames) => -((delta * numFrames) % interv);
 
-    public static long CalcCycleGroup(BigRational interv, BigRational delta, BigRational off) => CalcUtils.RMod((long) BigRational.Ceiling(off / delta), CalcCycleLength(interv, delta));
+    public static long CalcBaseLength(BigRational thresh, BigRational delta) => (long) BigRational.Floor(thresh / BigRational.Abs(delta));
+    public static BigRational CalcLengthOffset(BigRational thresh, BigRational delta) => thresh % BigRational.Abs(delta);
 
     public static void DescendRecursionLevels(ref BigRational interv, ref BigRational delta, int recLevel) {
         BigRational off = default;
